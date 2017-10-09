11f61cecf6c0cd972da66581a04226c45392dae1
diegokim <diegofk26@gmail.com>
2017-10-09 17:51:41 -0300

    new message view update

--- app/src/main/java/com/example/android/linkup/links/LinksAdapter.java
+++ app/src/main/java/com/example/android/linkup/links/LinksAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -20,6 +21,7 @@ import com.example.android.linkup.models.Profile;
 import com.example.android.linkup.models.Session;
 import com.example.android.linkup.network.WebServiceManager;
 import com.example.android.linkup.utils.Base64Converter;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -28,6 +30,8 @@ import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 public class LinksAdapter extends RecyclerView.Adapter<LinksViewHolder>{
@@ -58,8 +62,8 @@ public class LinksAdapter extends RecyclerView.Adapter<LinksViewHolder>{
         holder.profilePhoto.setImageBitmap(photo);
         holder.header.setText(profile.name + ", " + Integer.toString(profile.age));
         // TODO: Set the real id
-        String myId = Session.getInstance().myProfile.id;
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("chats/"+myId+"/messages/"+profile.id);
+        final String myId = Session.getInstance().myProfile.id;
+        final DatabaseReference ref = FirebaseDatabase.getInstance().getReference("chats/"+myId+"/messages/"+profile.id);
 
         Query lastQuery = ref.orderByKey().limitToLast(1);
 
@@ -73,6 +77,17 @@ public class LinksAdapter extends RecyclerView.Adapter<LinksViewHolder>{
                         //Log.d("user key",child.getKey());
                         //Log.d("user val",child.child("message").getValue().toString());
                         holder.lastMessage.setText(child.child("message").getValue().toString());
+                        String lastMessageUser = child.child("messageUser").getValue().toString();
+                        if (! lastMessageUser.equals(Session.getInstance().myProfile.name)) {
+                            holder.lastMessage.setText(lastMessageUser + ": " + child.child("message").getValue().toString());
+                            Boolean viewed = (Boolean) child.child("viewed").getValue();
+                            if (! viewed ) {
+                                holder.lastMessage.setTypeface(null, Typeface.BOLD_ITALIC);
+                            }
+                        } else {
+                            holder.lastMessage.setText("yo: " + child.child("message").getValue().toString());
+                        }
+
                     }
                 }
             }
@@ -83,11 +98,55 @@ public class LinksAdapter extends RecyclerView.Adapter<LinksViewHolder>{
             }
         });
 
+//        ref.addChildEventListener(new ChildEventListener() {
+//            @Override
+//            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+//                Log.d("ASDJ","ASLKDJ");
+//            }
+//
+//            @Override
+//            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+//
+//            }
+//
+//            @Override
+//            public void onChildRemoved(DataSnapshot dataSnapshot) {
+//
+//            }
+//
+//            @Override
+//            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//
+//            }
+//        });
+
         holder.moreVert.setOnClickListener(new LinkManagementPopUpClickListener(profile));
         holder.view.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(final View view) {
                 ActiveChatProfile.getInstance().update(profile);
+                DatabaseReference ref = FirebaseDatabase.getInstance().getReference("chats/"+ myId +"/messages/"+profile.id);
+                ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        DatabaseReference innerRef = FirebaseDatabase.getInstance().getReference("chats/"+ myId +"/messages/"+profile.id);
+                        for (DataSnapshot child : dataSnapshot.getChildren()) {
+                            String key = child.getKey();
+                            innerRef.child(key).child("viewed").setValue(true);
+                        }
+                        innerRef.removeEventListener(this);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
                 context.startActivity(new Intent(context, ChatActivity.class));
             }
         });
