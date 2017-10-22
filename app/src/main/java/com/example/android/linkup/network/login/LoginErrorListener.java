9c7797095bad695a161a39bf8344de134ce8eac8
Lucas Ludueño <ludueno.lucas@gmail.com>
2017-10-22 19:15:50 -0300

    Add message when user has been blocked and try to login #71 (#74)

--- app/src/main/java/com/example/android/linkup/network/login/LoginErrorListener.java
+++ app/src/main/java/com/example/android/linkup/network/login/LoginErrorListener.java
@@ -2,11 +2,9 @@ package com.example.android.linkup.network.login;
 
 
 import android.util.Log;
-import android.widget.Toast;
 
 import com.android.volley.NetworkResponse;
 import com.android.volley.Response;
-import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
 import com.example.android.linkup.network.NetworkErrorMessages;
 import com.example.android.linkup.network.WebServiceManager;
@@ -24,7 +22,14 @@ public class LoginErrorListener implements Response.ErrorListener {
     @Override
     public void onErrorResponse(VolleyError error) {
         Log.e(NetworkErrorMessages.LOGIN_TAG, error.toString());
-        String message = NetworkErrorMessages.ERROR_COMMUNICATING_WITH_THE_SERVER;
+
+        String message;
+
+        if (error.networkResponse.statusCode == 403) {
+            message = NetworkErrorMessages.ERROR_BLOCKED_ACCOUNT;
+        } else {
+            message = NetworkErrorMessages.ERROR_COMMUNICATING_WITH_THE_SERVER;
+        }
 
         NetworkResponse response = error.networkResponse;
         if(response != null && response.data != null ) {
